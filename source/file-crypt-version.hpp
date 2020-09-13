53963dcab91bdca28603e393623f679897ff9ec9
Grigore Stefan <g_stefan@yahoo.com>
2020-09-14 00:22:17 +0300

    update

--- source/file-crypt-version.hpp
+++ source/file-crypt-version.hpp
@@ -10,10 +10,10 @@
 #ifndef FILE_CRYPT_VERSION_HPP
 #define FILE_CRYPT_VERSION_HPP
 
-#define FILE_CRYPT_VERSION_ABCD                1,0,0,2
-#define FILE_CRYPT_VERSION_STR                 "1.0.0"
-#define FILE_CRYPT_VERSION_STR_BUILD           "2"
-#define FILE_CRYPT_VERSION_STR_DATETIME        "2020-02-24 03:06:44"
+#define FILE_CRYPT_VERSION_ABCD                1,1,0,3
+#define FILE_CRYPT_VERSION_STR                 "1.1.0"
+#define FILE_CRYPT_VERSION_STR_BUILD           "3"
+#define FILE_CRYPT_VERSION_STR_DATETIME        "2020-09-14 00:18:09"
 
 #ifndef XYO_RC
 
