eb3034893eeb70201cc4387646e1bc2c851a653e
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:40:49 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,11 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update file-crypt
+
+xyo-cc --update-version-dependency file-crypt
+
+
+
