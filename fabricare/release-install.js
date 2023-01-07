7cd03fa53e966459b67b283ca7ca709621667f25
Grigore Stefan <g_stefan@yahoo.com>
2023-01-08 00:59:12 +0200

    Update

---
+++ fabricare/release-install.js
@@ -0,0 +1,42 @@
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+// Public domain (Unlicense) <http://unlicense.org>
+// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-License-Identifier: Unlicense
+
+if (!Script.isNil(Solution.hasRelease)) {
+	if (!Solution.hasRelease) {
+		return;
+	};
+};
+
+// ---
+
+messageAction("release-install");
+
+var pathSeparator = "/";
+if (OS.isWindows()) {
+	if (Platform.name.indexOf("mingw") >= 0) {
+		pathSeparator = "/";
+	} else {
+		pathSeparator = "\\";
+	};
+};
+
+var version = getVersion();
+
+var releasePrefix = Solution.name;
+if (!Script.isNil(Solution.releaseName)) {
+	releasePrefix = Solution.releaseName;
+};
+
+var releaseName = releasePrefix + "-" + version + "-" + Platform.name;
+var jsonFilename = "release" + pathSeparator + releasePrefix + "-" + version + ".sha512.json";
+
+Shell.mkdirRecursivelyIfNotExists(global.pathRelease);
+
+Console.writeLn(global.pathRelease);
+Console.writeLn("release" + pathSeparator + releaseName + ".7z");
+Console.writeLn("release" + pathSeparator + releaseName + "-dev.7z");
+copyFileIfExists("release" + pathSeparator + releaseName + ".7z", global.pathRelease);
+copyFileIfExists("release" + pathSeparator + releaseName + "-dev.7z", global.pathRelease);
+copyFileIfExists(jsonFilename, global.pathRelease);
